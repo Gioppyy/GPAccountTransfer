5f36268e1a4a1723ac5816df87c6c957810e7310
Gioppyy <Giopanno006.com@gmail.com>
2026-01-12 14:11:41 +0100

    Formatted lang

--- main.py
+++ main.py
@@ -21,36 +21,50 @@ def main():
         logger.error(str(e))
 
     cfg = Config(logger)
-    logger.info("Config initialized")
-    logger.info(f"Server PATH: {cfg["server_path"]}")
-    logger.info(f"old username: {old_username}")
-    input_utils.check_ans(logger, input("Correct (y/n)? "))
-    logger.info(f"new username: {new_username}")
-    input_utils.check_ans(logger, input("Correct (y/n)? "))
+    logger.info("Configuration initialized")
+    logger.info(f"Server path: {cfg['server_path']}")
+    logger.info(f"Old username: {old_username}")
+    input_utils.check_ans(logger, input("Is this correct? (y/n): "))
+    logger.info(f"New username: {new_username}")
+    input_utils.check_ans(logger, input("Is this correct? (y/n): "))
 
-    if (old_username == new_username):
-        logger.critical("Please insert different usernames")
+    if old_username == new_username:
+        logger.critical("Please provide different usernames")
 
     if bedrock:
         old_username = cfg["bedrock_format"].replace("{username}", old_username)
         new_username = cfg["bedrock_format"].replace("{username}", new_username)
-        logger.info(f"formatted old username for bedrock user: {old_username}")
-        logger.info(f"formatted new username for bedrock user: {new_username}")
+        logger.info(f"Formatted old username for Bedrock user: {old_username}")
+        logger.info(f"Formatted new username for Bedrock user: {new_username}")
 
-    logger.info("Getting and generating uuid")
+    logger.info("Generating UUIDs")
     uuid_utils = UUIDUtils(logger)
     old_uuid = uuid_utils.generate_offline_uuid(old_username)
     new_uuid = uuid_utils.generate_offline_uuid(new_username)
-    logger.info("Generated successfully")
+    logger.info("UUIDs generated successfully")
 
     logs = []
-    fscanner = FolderScanner(logger, cfg["server_path"], old_uuid, old_username, new_uuid, new_username, dry_run)
+    fscanner = FolderScanner(
+        logger,
+        cfg["server_path"],
+        old_uuid,
+        old_username,
+        new_uuid,
+        new_username,
+        dry_run
+    )
     logs.extend(fscanner.scan())
 
     if cfg["storage_type"] != "none":
         try:
-            logger.info("Scanning database")
-            mysql_scanner = MySQLScanner(logger, cfg["db_host"], cfg["db_port"], cfg["db_username"], cfg["db_password"])
+            logger.info("Scanning database...")
+            mysql_scanner = MySQLScanner(
+                logger,
+                cfg["db_host"],
+                cfg["db_port"],
+                cfg["db_username"],
+                cfg["db_password"]
+            )
             mysql_logs = mysql_scanner.scan_all_databases(old_uuid, old_username)
             mysql_scanner.update_entries(
                 mysql_logs,
@@ -62,15 +76,17 @@ def main():
             )
             logs.extend(mysql_logs)
         except Exception as e:
-            logger.error(f"Cant connect to MySQL server: {e}")
-    else: logger.info("Database scan is disabled.. skipping")
+            logger.error(f"Cannot connect to MySQL server: {e}")
+    else:
+        logger.info("Database scanning is disabled, skipping...")
 
     if cfg["backup_enabled"]:
         backup = Backupper(logger, cfg["backup_path"])
         backup.log_changes(logs)
-    else: logger.info("Backup is disabled.. skipping")
+    else:
+        logger.info("Backup is disabled, skipping...")
 
-    logger.info(f"Found and updated {len(logs)} entry for old name / uuid")
+    logger.info(f"Found and updated {len(logs)} entries for old username/UUID")
 
 if __name__ == "__main__":
     main()
